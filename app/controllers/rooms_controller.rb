68e058f78ed033303e2dae07b668c6476564592e
musashi-634 <kazuaki.miyamoto.job@gmail.com>
2023-02-03 15:08:36 +0900

    【追加】Read処理を実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: %i[show edit update destroy]
+
   def index
     @rooms = Room.all
   end
@@ -29,5 +31,11 @@ class RoomsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
