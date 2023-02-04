88f4efc15ada781b3abf4951a18d9462848ee463
musashi-634 <kazuaki.miyamoto.job@gmail.com>
2023-02-04 16:37:23 +0900

    【追加】エリア名による検索機能を実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   before_action :set_room, only: %i[show edit update]
 
   def index
-    @rooms = Room.all
+    @rooms = Room.where('address LIKE ?', "%#{params[:address]}%")
   end
 
   def new
